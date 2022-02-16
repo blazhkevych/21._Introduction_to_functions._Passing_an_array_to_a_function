811c861b76a35bc92fdda4a7d9f1e24763554259
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-02-16 12:27:09 +0200

    +4 задание

--- task_4/task_4.cpp
+++ task_4/task_4.cpp
@@ -3,14 +3,31 @@
 параметров 2 целых числа и возвращает сумму чисел из
 диапазона между ними.
 */
-// НЕ РАБОТАЕТ
 
 #include <iostream>
 using namespace std;
 
+int SumOfNumbersBetweenStartEnd(int start, int end) // Функция, вычисляет сумму чисел из диапазона между ними.
+{
+	int sum{ 0 }, i = start + 1;
+	while (i < end)
+	{
+		sum += i;
+		i++;
+	}
+	return sum;
+}
+
 int main()
 {
 	setlocale(LC_ALL, ""); // Работает только с потоком вывода.
+
+	cout << "Введите первое число, затем второе: \n";
+	int num1{ 0 }, num2{ 0 };
+	cin >> num1 >> num2;
+	cout << endl;
+
+	cout << "Cумма чисел из диапазона между ними : \n" << SumOfNumbersBetweenStartEnd(num1, num2) << endl;
 
 	return 0;
 }
