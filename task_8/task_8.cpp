262e1f9fbe1225786997c785f0558a7c70223578
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-02-19 19:18:14 +0200

    +8 задание

--- task_8/task_8.cpp
+++ task_8/task_8.cpp
@@ -5,7 +5,6 @@
 задачи необходимо также написать функцию, которая
 определяет, является ли год високосным.
 */
-// НЕ РАБОТАЕТ
 
 /*
 Возможно, стоит еще дописать ф-ю сравнения введенных дат на то, чтобы вторая дата была больше первой.
@@ -40,74 +39,40 @@ enum Months
 
 bool DateValidationCheck(int day, int month, int year) // Функция, проверяющая корректность введенных значений даты.
 {
-
-	//cout << "Введите год ( > 1 ):" << endl;
-	//cin >> year;
 	if (year > 0)
 	{
-		//cout << "Введите месяц ( > 0 и < 13):" << endl;
-		//cin >> month;
 		if (month > 0 && month < 13)
 		{
-			//cout << "Введите день ( > 0 и < 32):" << endl;
-			//cin >> day;
 			if (
 				((IsItALeapYear(year))
-					&& (month == February) && (day == 29)) // если 29 февраля в високосном году
+					&& (month == February) && (day == 29)) // Если 29 февраля в високосном году.
 				|| ((IsItALeapYear(year))
-					&& (month == February) && (day > 0 && day < 29)) // если 1-28 февраля, високосный год
+					&& (month == February) && (day > 0 && day < 29)) // Если 1-28 февраля, високосный год.
 				|| (!(IsItALeapYear(year))
-					&& (month == February) && (day > 0 && day < 29)) // если 1-28 февраля, обычный год
+					&& (month == February) && (day > 0 && day < 29)) // Если 1-28 февраля, обычный год.
 				|| ((month == January
 					|| month == March
 					|| month == May
 					|| month == July
 					|| month == August
 					|| month == October
-					|| month == December) && (day > 0 && day < 32)) // все месяца, в которых 31 день
+					|| month == December) && (day > 0 && day < 32)) // Все месяца, в которых 31 день.
 				|| ((month == April
 					|| month == June
 					|| month == September
-					|| month == November) && (day > 0 && day < 31)) // все месяца, в которых 30 дней
+					|| month == November) && (day > 0 && day < 31)) // Все месяца, в которых 30 дней.
 				)
 			{
-				//cout << "Введите часы ( > -1 и < 24 ):" << endl;
-				//cin >> time_hours;
-				//if (time_hours > -1 && time_hours < 24)
-				//{
-					//cout << "Введите минуты ( > -1 и < 60 ):" << endl;
-					//cin >> time_minutes;
-					//if (time_minutes > -1 && time_minutes < 60)
-					//{
-						//cout << "Введите секунды( > 0 и < 60 ):" << endl;
-						//cin >> time_seconds;
-						//if (time_seconds > 0 && time_seconds < 60)
-						//{
-				return true;// cout << "Все введённые значения корректны."
-								//<< "\nДата: " << day << '.' << month << '.' << year;
-							//if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
-								//cout << " (високосный год)." << endl;
-							//else
-							//	cout << " (обычный год)." << endl;
-							//cout << "\nВремя: " << time_hours << ':' << time_minutes << ':' << time_seconds << endl;
-						//}
-						//else
-							//cout << "Введите корректные секунды." << endl;
-					//}
-					//else
-						//cout << "Введите корректные минуты." << endl;
-				//}
-				//else
-					//cout << "Введите корректные часы." << endl;
+				return true; // Все введённые значения корректны.
 			}
 			else
-				return false;//cout << "Введите корректный день." << endl;
+				return false; // Введен не корректный день.
 		}
 		else
-			return false;//cout << "Введите корректный месяц." << endl;
+			return false; // Введен не корректный месяц. 
 	}
 	else
-		return false;//cout << "Введите корректный год." << endl;
+		return false; // Введен не корректный год. 
 }
 
 int DifferenceInDaysBetweenDates(
@@ -122,7 +87,7 @@ int DifferenceInDaysBetweenDates(
 	}
 	else
 	{
-		while (year1 != year2 && month1 != month2 && day1 != day2) // Пока даты не равны, вычисляем следующую дату.
+		while (!(year1 == year2 && month1 == month2 && day1 == day2)) // Пока даты не равны, вычисляем следующую дату.
 		{
 			switch (month1)
 			{
@@ -197,49 +162,12 @@ int DifferenceInDaysBetweenDates(
 int main()
 {
 	setlocale(LC_ALL, ""); // Работает только с потоком вывода.
-/*cout << boolalpha << IsItALeapYear(15) << endl; // Задачу выполняет исправно, тесты прошла.
-		cout << boolalpha << IsItALeapYear(1700) << endl;
-		cout << boolalpha << IsItALeapYear(1800) << endl;
-		cout << boolalpha << IsItALeapYear(2007) << endl;
-		cout << boolalpha << IsItALeapYear(2021) << endl;
-		cout << boolalpha << IsItALeapYear(2022) << endl;*/
 
-	//Допроверить работу функций DateValidationCheck и DifferenceInDaysBetweenDates
-	cout << "True:\n";
-	cout << boolalpha << DateValidationCheck(29, 2, 2001) << endl;
-	cout << "False:\n";
+	int day1{ 1 }, month1{ 2 }, year1{ 2022 }, day2{ 19 }, month2{ 2 }, year2{ 2022 };
+	cout << "Разница в дня между "
+		<< day1 << '.' << month1 << '.' << year1 << " и "
+		<< day2 << '.' << month2 << '.' << year2
+		<< " составляет " << DifferenceInDaysBetweenDates(day1, month1, year1, day2, month2, year2) << " дней." << endl;
 
-	// https://github.com/blazhkevych/8_Conditional_Operator_IF_Operator_SWITCH/blob/master/task_4/task_4.cpp
-		
-
-
-
-
-
-
-
-
-
-
-
-
-		/*int day1{ 0 }, month1{ 0 }, year1{ 0 };
-		while (DateValidationCheck(day1, month1, year1) == false)
-		{
-			cout << "Введите 1-ю дату, сначала день, месяц, затем год (22.9.2019): \n";
-			cin >> day1 >> month1 >> year1;
-			cout << endl;
-		}
-
-		int day2{ 0 }, month2{ 0 }, year2{ 0 };
-		while (DateValidationCheck(day2, month2, year2) == false)
-		{
-			cout << "Введите 2-ю дату, сначала день, месяц, затем год (1.10.2021): \n";
-			cin >> day2 >> month2 >> year2;
-			cout << endl;
-		}
-
-		cout << "Разность в днях между этими датами = " << DifferenceInDaysBetweenDates(day1, month1, year1, day2, month2, year2);*/
-
-		return 0;
+	return 0;
 }
